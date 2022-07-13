95fea3b2f18f9fb27a0ca9a28587103aa8ff7656
Brian Day <22bday@gmail.com>
2022-07-13 14:41:50 -0400

    Added arg parser.

--- birdday/kpoints-convergence/kpoint.py
+++ birdday/kpoints-convergence/kpoint.py
@@ -1,4 +1,22 @@
+import argparse
 import numpy as np
+import warnings
+
+
+# Parse arguments from command line.
+# -h, --help is added by default.
+parser = argparse.ArgumentParser()
+parser.add_argument('input', help="Path to Quantum Espresso input file.", type=str)
+parser.add_argument('ke_cutoff', help="Desired kinetic energy cutoff. Default units is eV.", type=float)
+parser.add_argument('-u', '--units', help="Set units for ke_cutoff.", type=str)
+
+args = parser.parse_args()
+if args.units:
+    conversion_factors = {'eV':1, 'meV':1e3, 'Ha':27.2114, 'Ry':13.6057}
+    if args.units not in conversion_factors.keys():
+        raise argparse.ArgumentTypeError('Units must be one of the following: eV, meV, Ha, or Ry.')
+    else:
+        print(f'{args.ke_cutoff} {args.units} <--> {args.ke_cutoff*conversion_factors[args.units]} eV')
 
 
 class ConvTracker:
