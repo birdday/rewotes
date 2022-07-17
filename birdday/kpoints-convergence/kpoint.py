3418cc8a17a799753515f2c7ed37fc57d24e945a
Brian Day <22bday@gmail.com>
2022-07-16 21:30:03 -0400

    Updated class attributes to include needed ids and endpoints.

--- birdday/kpoints-convergence/kpoint.py
+++ birdday/kpoints-convergence/kpoint.py
@@ -6,22 +6,23 @@ import warnings
 
 class ConvTracker:
 
-    def __init__(self, file, ke_cutoff):
-        self.input_file = file          # Path to Quantum Espresso input file
-        self.cycle_number = int(0)      # Uses 0 indexing
-        self.energy_cutoff = ke_cutoff  # Units = eV
-        self.mesh = [1,1,1]             # k-point mesh
-        self.ke_vals = []               # Used to track all energies
+    def __init__(self, owner_id, project_id, material_id, workflow_id, job_endpoints, cutoff=1e-5, energy=[]):
+        self.owner_id = owner_id
+        self.project_id = project_id
+        self.material_id = material_id
+        self.workflow_id = workflow_id
+        self.job_endpoints = job_endpoints
+        self.cutoff = cutoff            # Units = eV
+        self.energy = energy            # Array of energies can be passed in to continue a job set.
 
         self.parse_input()
 
 
     def check_convergence(self):
-        if len(self.ke_vals) < 2:
-            warnings.warn('Not enough recorded energies, returning False.')
+        if len(self.energy) < 2:
             return False
         else:
-            return (self.ke_vals[self.cycle_number] - self.ke_vals[self.cycle_number]-1) <= self.energy_cutoff
+            return abs(self.energy[-1] - self.energy[-2]) <= self.cutoff
 
     def update_mesh(self):
         self.mesh = [val+1 for val in self.mesh]
