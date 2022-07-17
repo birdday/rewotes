35177a7cf43e64b8051d4d3083edac8cc0bca12b
Brian Day <22bday@gmail.com>
2022-07-16 21:35:48 -0400

    Added job creation/submission method, output parsing method.

--- birdday/kpoints-convergence/kpoint.py
+++ birdday/kpoints-convergence/kpoint.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import warnings
 
+import urllib.request
 
 
 class ConvTracker:
@@ -15,7 +16,37 @@ class ConvTracker:
         self.cutoff = cutoff            # Units = eV
         self.energy = energy            # Array of energies can be passed in to continue a job set.
 
+    def create_submit_job(self, kp, jobs_set=None):
+        JOB_NAME = f"kpoint_{kp}"
+        config = {
+            "owner": {"_id": self.owner_id},
+            "_material": {"_id": self.material_id},
+            "workflow": {"_id": self.workflow_id},
+            "name": JOB_NAME
+        }
+        job = job_endpoints.create(config)
+        if jobs_set is not None:
+            job_endpoints.move_to_set(job["_id"], "", jobs_set["_id"])
 
+        # Update K-Point Values
+        # This is not an ideal way to set kpoints, but the built in convergence tool did npt work as expected, and adjusting the workflow did not update render.
+        job["workflow"]["subworkflows"][0]["units"][0]["input"][0]["rendered"] = job["workflow"]["subworkflows"][0]["units"][0]["input"][0]["rendered"].replace("K_POINTS automatic\n10 10 10 0 0 0", f"K_POINTS automatic\n{kp} {kp} {kp} 0 0 0")
+        job_endpoints.update(job["_id"], job)
+        job_endpoints.submit(job['_id'])
+
+        return job["_id"]
+
+    def parse_output(self, job_id):
+        files = self.job_endpoints.list_files(job_id)
+        output_file = [file for file in files if file["name"] ==  'pw_scf.out'][0]
+        server_response = urllib.request.urlopen(output_file['signedUrl'])
+        output_file_bytes = server_response.read()
+        output_file = output_file_bytes.decode(encoding="UTF-8")
+        output_as_array = output_file.split("\n")
+        total_energy_ry = float(re.split(" +", [row for row in output_as_array if "!    total energy" in row][0])[-2])
+        total_energy_ev = total_energy_ry * 13.6056980659
+
+        return total_energy_ev
 
     def check_convergence(self):
         if len(self.energy) < 2:
